e937802cf866e2da3d6d41f947799602f0944d25
Erik Garrison <erik.garrison@gmail.com>
2020-06-21 10:29:47 +0200

    multithreaded PAF output with cigars for mashmap-align

--- src/align/include/computeAlignments.hpp
+++ src/align/include/computeAlignments.hpp
@@ -11,8 +11,10 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <zlib.h>  
+#include <sstream>
+#include <zlib.h>
 #include <cassert>
+#include <thread>
 
 //Own includes
 #include "align/include/align_types.hpp"
@@ -23,11 +25,28 @@
 //External includes
 #include "common/kseq.h"
 #include "common/edlib.h"
+#include "common/atomic_queue/atomic_queue.h"
 
 KSEQ_INIT(gzFile, gzread)
 
 namespace align
 {
+
+  struct seq_record_t {
+      MappingBoundaryRow currentRecord;
+      std::string mappingRecordLine;
+      std::string qSequence;
+      seq_record_t(const MappingBoundaryRow& c, const std::string& r, const std::string& q)
+          : currentRecord(c)
+          , mappingRecordLine(r)
+          , qSequence(q)
+          { }
+  };
+  // load into this
+  typedef atomic_queue::AtomicQueue<seq_record_t*, 2 << 16> seq_atomic_queue_t;
+  // results into this, write out
+  typedef atomic_queue::AtomicQueue<std::string*, 2 << 16> paf_atomic_queue_t;
+
   /**
    * @class     align::Aligner
    * @brief     compute alignments and generate sam output
@@ -110,81 +129,181 @@ namespace align
        */
       void computeAlignments()
       {
-        //Parse query sequences
-        for(const auto &fileName : param.querySequences)
-        {
-#ifdef DEBUG
-          std::cout << "INFO, align::Aligner::computeAlignments, parsing query sequences in file " << fileName << std::endl;
-#endif
 
-          //Open the file using kseq
-          FILE *file = fopen(fileName.c_str(), "r");
-          gzFile fp = gzdopen(fileno(file), "r");
-          kseq_t *seq = kseq_init(fp);
+          // input atomic queue
+          seq_atomic_queue_t seq_queue;
+          // output atomic queue
+          paf_atomic_queue_t paf_queue;
+          // flag when we're done reading
+          std::atomic<bool> reader_done;
+          reader_done.store(false);
 
-          //Open mashmap output file
-          std::ifstream mappingListStream(param.mashmapPafFile);
-          std::string mappingRecordLine;
-          MappingBoundaryRow currentRecord;
+          auto& nthreads = param.threads;
 
-          //size of sequence
-          skch::offset_t len;
-
-          std::ofstream outstrm(param.samOutputFile);
-
-          while ((len = kseq_read(seq)) >= 0) 
-          {
-             std::string qSeqId = seq->name.s;
-
-             skch::CommonFunc::makeUpperCase(seq->seq.s, len);
-             std::string qSequence = seq->seq.s;
-
-             //Check if all mapping records are processed already
-             if( mappingListStream.eof() )
-               break;
-
-             //Read first record from mashmap output file during first iteration
-             if(mappingRecordLine.empty())       
-             {
-               std::getline(mappingListStream, mappingRecordLine);
-             }
-
-             this->parseMashmapRow(mappingRecordLine, currentRecord);
-
-             //Check if mapping query id matches current query sequence id
-             if(currentRecord.qId != qSeqId)
-             {
-               //Continue to read the next query sequence
-               continue;
-             }
-             else
-             {
-               this->doAlignment(currentRecord, mappingRecordLine, qSequence, outstrm);
-             }
-
-             //Check if more mappings have same query sequence id
-             while(std::getline(mappingListStream, mappingRecordLine))
-             {
-               this->parseMashmapRow(mappingRecordLine, currentRecord);
-
-               if(currentRecord.qId != qSeqId)
-               {
-                 //Break the inner loop to read query sequence
-                 break;
-               }
-               else
-               {
-                 this->doAlignment(currentRecord, mappingRecordLine, qSequence, outstrm);
-               }
-             }
+          // atomics to record if we're working or not
+          std::vector<std::atomic<bool>> working(nthreads);
+          for (auto& w : working) {
+              w.store(true);
           }
 
-          mappingListStream.close();
+          // reader picks up candidate alignments from input
+          auto reader_thread =
+              [&](void) {
+                  //Parse query sequences
+                  for(const auto &fileName : param.querySequences)
+                  {
+#ifdef DEBUG
+                      std::cout << "INFO, align::Aligner::computeAlignments, parsing query sequences in file " << fileName << std::endl;
+#endif
 
-          kseq_destroy(seq);  
-          gzclose(fp); //close the file handler 
-          fclose(file);
-        }
+                      //Open the file using kseq
+                      FILE *file = fopen(fileName.c_str(), "r");
+                      gzFile fp = gzdopen(fileno(file), "r");
+                      kseq_t *seq = kseq_init(fp);
+
+                      //Open mashmap output file
+                      std::ifstream mappingListStream(param.mashmapPafFile);
+                      std::string mappingRecordLine;
+                      MappingBoundaryRow currentRecord;
+
+                      //size of sequence
+                      skch::offset_t len;
+
+                      while ((len = kseq_read(seq)) >= 0) 
+                      {
+                          std::string qSeqId = seq->name.s;
+
+                          skch::CommonFunc::makeUpperCase(seq->seq.s, len);
+                          std::string qSequence = seq->seq.s;
+
+                          //Check if all mapping records are processed already
+                          if( mappingListStream.eof() )
+                              break;
+
+                          //Read first record from mashmap output file during first iteration
+                          if(mappingRecordLine.empty())       
+                          {
+                              std::getline(mappingListStream, mappingRecordLine);
+                          }
+
+                          this->parseMashmapRow(mappingRecordLine, currentRecord);
+
+                          //Check if mapping query id matches current query sequence id
+                          if(currentRecord.qId != qSeqId)
+                          {
+                              //Continue to read the next query sequence
+                              continue;
+                          }
+                          else
+                          {
+                              // XXX TODO
+                              //this->doAlignment(currentRecord, mappingRecordLine, qSequence, outstrm);
+                              auto q = new seq_record_t(currentRecord, mappingRecordLine, qSequence);
+                              seq_queue.push(q);
+                          }
+
+                          //Check if more mappings have same query sequence id
+                          while(std::getline(mappingListStream, mappingRecordLine))
+                          {
+                              this->parseMashmapRow(mappingRecordLine, currentRecord);
+
+                              if(currentRecord.qId != qSeqId)
+                              {
+                                  //Break the inner loop to read query sequence
+                                  break;
+                              }
+                              else
+                              {
+                                  // XXX TODO
+                                  //this->doAlignment(currentRecord, mappingRecordLine, qSequence, outstrm);
+                                  auto q = new seq_record_t(currentRecord, mappingRecordLine, qSequence);
+                                  seq_queue.push(q);
+                              }
+                          }
+                      }
+
+                      mappingListStream.close();
+
+                      kseq_destroy(seq);  
+                      gzclose(fp); //close the file handler 
+                      fclose(file);
+                  }
+                  reader_done.store(true);
+              };
+
+          // helper to check if we're still aligning
+          auto still_working =
+              [&](const std::vector<std::atomic<bool>>& working) {
+                  bool ongoing = false;
+                  for (auto& w : working) {
+                      ongoing = ongoing || w.load();
+                  }
+                  return ongoing;
+              };
+
+          // writer, picks output from queue and writes it to our output stream
+          std::ofstream outstrm(param.samOutputFile);
+          auto writer_thread =
+              [&](void) {
+                  while (true) {
+                      std::string* paf_lines = nullptr;
+                      if (!paf_queue.try_pop(paf_lines)
+                          && !still_working(working)) {
+                          break;
+                      } else if (paf_lines != nullptr) {
+                          outstrm << *paf_lines;
+                          delete paf_lines;
+                      } else {
+                          std::this_thread::sleep_for(100ns);
+                      }
+                  }
+              };
+
+          // worker, takes candidate alignments and runs edlib alignment on them
+          auto worker_thread = 
+              [&](uint64_t tid,
+                  std::atomic<bool>& is_working) {
+                  is_working.store(true);
+                  while (true) {
+                      seq_record_t* rec = nullptr;
+                      if (!seq_queue.try_pop(rec)
+                          && reader_done.load()) {
+                          break;
+                      } else if (rec != nullptr) {
+                          std::string* paf_rec
+                              = new std::string(
+                                  doAlignment(rec->currentRecord,
+                                              rec->mappingRecordLine,
+                                              rec->qSequence));
+                          paf_queue.push(paf_rec);
+                          delete rec;
+                      } else {
+                          std::this_thread::sleep_for(100ns);
+                      }
+                  }
+                  is_working.store(false);
+              };
+
+          // launch reader
+          std::thread reader(reader_thread);
+          // launch writer
+          std::thread writer(writer_thread);
+          // launch workers
+          std::vector<std::thread> workers; workers.reserve(nthreads);
+          for (uint64_t t = 0; t < nthreads; ++t) {
+              workers.emplace_back(worker_thread,
+                                   t,
+                                   std::ref(working[t]));
+          }
+
+          // wait for reader and workers to complete
+          reader.join();
+          for (auto& worker : workers) {
+              worker.join();
+          }
+          // and finally the writer
+          writer.join();
+
       }
 
       /**
@@ -224,7 +343,7 @@ namespace align
        * @param[in]   qSequence           query sequence
        * @param[in]   outstrm             output stream
        */
-      void doAlignment(MappingBoundaryRow &currentRecord, const std::string &mappingRecordLine, const std::string &qSequence, std::ofstream &outstrm)
+      std::string doAlignment(MappingBoundaryRow &currentRecord, const std::string &mappingRecordLine, const std::string &qSequence)
       {
 
 #ifdef DEBUG
@@ -286,23 +405,26 @@ namespace align
         std::cout << "INFO, align::Aligner::doAlignment, time spent= " << timeAlign.count()  << " sec" << std::endl;
 #endif
 
+        std::stringstream output;
         //Output to file
         if (result.status == EDLIB_STATUS_OK && result.alignmentLength != 0) 
         {
           char* cigar = edlibAlignmentToCigar(result.alignment, result.alignmentLength, EDLIB_CIGAR_STANDARD);
 
-          outstrm << mappingRecordLine
-                  << "\t" << "ed:i:" << result.editDistance
-                  << "\t" << "al:i" << result.alignmentLength
-                  << "\t" << "ad:f:" << result.editDistance * 1.0/result.alignmentLength
-                  << "\t" << "cg:Z:" << cigar
-                  << "\n";
+          output << mappingRecordLine
+                 << "\t" << "ed:i:" << result.editDistance
+                 << "\t" << "al:i" << result.alignmentLength
+                 << "\t" << "ad:f:" << result.editDistance * 1.0/result.alignmentLength
+                 << "\t" << "cg:Z:" << cigar
+                 << "\n";
 
           free(cigar);
         }
 
         edlibFreeAlignResult(result);
         delete [] queryRegionStrand;
+
+        return output.str();
       }
   };
 }
